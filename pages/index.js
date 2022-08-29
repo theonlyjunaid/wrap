3f865a46f272c538ac338d25f9046759df284458
Your Name <you@example.com>
2022-08-29 15:18:12 +0530

    sxz

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Navbar from "../components/Navbar"
 import Slide from "../components/Slide"
 import Footer from "../components/Footer"
 
+
 const Home = () => {
+
   return (
     <div>
       <Navbar />
