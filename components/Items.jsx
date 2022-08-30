4430d9faf9eab32c6d578e5409abb6a3fd93f63a
Your Name <you@example.com>
2022-08-30 14:14:02 +0530

    erd

--- components/Items.jsx
+++ components/Items.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
+import { getItemCategories } from '../service/Data'
+
+
+
 
 function Items() {
-    return (
-        <div className=' grid grid-cols-2 gap-4 p-4 md:grid-cols-3'>
-
-            <div className='mb-3'>
+    const Catogary = getItemCategories.map((item, index) => {
+        return (
+            <div className='mb-3' key={index}>
                 <Link href={`/post/${1 + 1}`}>
-                    <a > <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" className='hover:-hue-rotate-60' /></a>
+                    <a > <img src={item.Image} alt={item.Name}
+                        className='hover:-hue-rotate-60' /></a>
                 </Link>
             </div>
+        )
+    })
+    console.log(getItemCategories)
 
+    return (
+        <div className=' grid grid-cols-2 gap-4 p-4 md:grid-cols-3 md:m-6'>
+            {Catogary}
         </div>
     )
 }
