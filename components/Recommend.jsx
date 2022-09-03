6c6494684de8fa6ecd3e4394bed59b79c63b6239
Your Name <you@example.com>
2022-09-03 09:29:59 +0530

    fwds

--- components/Recommend.jsx
+++ components/Recommend.jsx
@@ -55,7 +55,7 @@ const FeaturedPosts = () => {
                 </div>
             </div>
             <div className='mb-3 border-red-300 border-2 '>
-                <Link href={`/post/${1 + 1}`}>
+                <Link href={`/post/product/${1 + 1}`}>
                     <a > <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" className='hover:-hue-rotate-60' /></a>
                 </Link>
                 <div>
@@ -66,7 +66,51 @@ const FeaturedPosts = () => {
                 </div>
             </div>
             <div className='mb-3 border-red-300 border-2 '>
-                <Link href={`/post/${1 + 1}`}>
+                <Link href={`/post/product/${1 + 1}`}>
+                    <a > <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" className='hover:-hue-rotate-60' /></a>
+                </Link>
+                <div>
+                    <p className='px-2 text-sm'></p>
+                    <div className='flex items-center'>
+                        <p className=' px-2 text-xs line-through'>&#8377; 450 </p>
+                        <p className=' px-2 text-sm '>&#8377; 250 </p></div>
+                </div>
+            </div>
+            <div className='mb-3 border-red-300 border-2 '>
+                <Link href={`/post/product/${1 + 1}`}>
+                    <a > <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" className='hover:-hue-rotate-60' /></a>
+                </Link>
+                <div>
+                    <p className='px-2 text-sm'></p>
+                    <div className='flex items-center'>
+                        <p className=' px-2 text-xs line-through'>&#8377; 450 </p>
+                        <p className=' px-2 text-sm '>&#8377; 250 </p></div>
+                </div>
+            </div>
+            <div className='mb-3 border-red-300 border-2 '>
+                <Link href={`/post/product/${1 + 1}`}>
+                    <a > <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" className='hover:-hue-rotate-60' /></a>
+                </Link>
+                <div>
+                    <p className='px-2 text-sm'></p>
+                    <div className='flex items-center'>
+                        <p className=' px-2 text-xs line-through'>&#8377; 450 </p>
+                        <p className=' px-2 text-sm '>&#8377; 250 </p></div>
+                </div>
+            </div>
+            <div className='mb-3 border-red-300 border-2 '>
+                <Link href={`/post/product/${1 + 1}`}>
+                    <a > <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" className='hover:-hue-rotate-60' /></a>
+                </Link>
+                <div>
+                    <p className='px-2 text-sm'></p>
+                    <div className='flex items-center'>
+                        <p className=' px-2 text-xs line-through'>&#8377; 450 </p>
+                        <p className=' px-2 text-sm '>&#8377; 250 </p></div>
+                </div>
+            </div>
+            <div className='mb-3 border-red-300 border-2 '>
+                <Link href={`/post/product/${1 + 1}`}>
                     <a > <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" className='hover:-hue-rotate-60' /></a>
                 </Link>
                 <div>
